4017ffb5a050e67ac8f523c00ee7f3b0027c2f3a
Wolfsblvt <wolfsblvt@gmx.de>
2015-05-01 01:08:52 +0200

    exclude vendor folders from coverage

--- travis/prepare-coverage.sh
+++ travis/prepare-coverage.sh
@@ -16,6 +16,6 @@ PHP_VERSION_FOR_COVERAGE=$4
 
 if [ "$TRAVIS_PHP_VERSION" == "$PHP_VERSION_FOR_COVERAGE" -a "$DB" == "mysqli" ]
 then
-	sed -n '1h;1!H;${;g;s/<\/php>/<\/php>\n\t<filter>\n\t\t<whitelist>\n\t\t\t<directory>..\/<\/directory>\n\t\t\t<exclude>\n\t\t\t\t<directory>..\/tests\/<\/directory>\n\t\t\t\t<directory>..\/language\/<\/directory>\n\t\t\t\t<directory>..\/migrations\/<\/directory>\n\t\t\t<\/exclude>\n\t\t<\/whitelist>\n\t<\/filter>/g;p;}' phpBB/ext/$EXTNAME/travis/phpunit-mysqli-travis.xml &> phpBB/ext/$EXTNAME/travis/phpunit-mysqli-travis.xml.bak
+	sed -n '1h;1!H;${;g;s/<\/php>/<\/php>\n\t<filter>\n\t\t<whitelist>\n\t\t\t<directory>..\/<\/directory>\n\t\t\t<exclude>\n\t\t\t\t<directory>..\/tests\/<\/directory>\n\t\t\t\t<directory>..\/vendor\/<\/directory>\n\t\t\t\t<directory>..\/lib\/<\/directory>\n\t\t\t\t<directory>..\/adm\/style\/lib\/<\/directory>\n\t\t\t\t<directory>..\/style\/all\/lib\/<\/directory>\n\t\t\t\t<directory>..\/language\/<\/directory>\n\t\t\t\t<directory>..\/migrations\/<\/directory>\n\t\t\t<\/exclude>\n\t\t<\/whitelist>\n\t<\/filter>/g;p;}' phpBB/ext/$EXTNAME/travis/phpunit-mysqli-travis.xml &> phpBB/ext/$EXTNAME/travis/phpunit-mysqli-travis.xml.bak
 	cp phpBB/ext/$EXTNAME/travis/phpunit-mysqli-travis.xml.bak phpBB/ext/$EXTNAME/travis/phpunit-mysqli-travis.xml
 fi
